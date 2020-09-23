6c86a26936c1c74e308f89a1a2698f7b868d6816
Justin Ma <justin.ma8419@gmail.com>
2020-09-22 22:47:15 -0400

    [Justin] Added JPG file to the data dir. The following def have been added: launching_website and sign_in.

---
+++ practice_run.py
@@ -0,0 +1,10 @@
+from web_functions import *
+
+url = data['url']
+username = data['username']
+password = data['password']
+
+launching_website(url)
+sign_in(username, password)
+
+
--- web_functions.py
+++ web_functions.py
@@ -1,20 +1,25 @@
-# from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.support.select import Select
-# from selenium.webdriver import ActionChains
-# import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+from selenium.webdriver import ActionChains
+from selenium.webdriver import ChromeOptions
+import time
 import yaml
 from os.path import dirname, abspath
 
-# driver = webdriver.Chrome()
-# driver.implicitly_wait(20)
-# driver.maximize_window()
+chrome_options = webdriver.ChromeOptions()
+prefs = {"profile.default_content_setting_values.notifications": 2}
+chrome_options.add_experimental_option("prefs", prefs)
 
+driver = webdriver.Chrome(chrome_options=chrome_options)
+driver.implicitly_wait(20)
+driver.maximize_window()
 
 ROOT_DIR = dirname(abspath(__file__))
 
 
 def load_yaml(filepath):
+    """Initiating safe loading the yaml file from dir path"""
     with open(filepath, 'r') as info:
         document = yaml.safe_load(info)
     return document
@@ -22,5 +27,28 @@ def load_yaml(filepath):
 
 data = load_yaml(f'{ROOT_DIR}/data/credentials.yml')
 
-url = data['url']
-print(url)
+
+def launching_website(url):
+    driver.get(url)
+    print(f"{url} is loading")
+
+
+def sign_in(username, password):
+    """Entering the email address"""
+    email_address = driver.find_element_by_xpath("//input[@placeholder='Email or Phone']")
+    email_address.clear()
+    email_address.send_keys(username)
+    print("Email is being entered as the user name")
+
+    """Entering the password"""
+    pw = driver.find_element_by_xpath("//input[@placeholder='Password']")
+    pw.clear()
+    pw.send_keys(password)
+    print("Password is entered")
+
+    """Clicking on the sing in button"""
+    sign_in_button = driver.find_element_by_xpath(
+        "//span[@class='d2edcug0 hpfvmrgz qv66sw1b c1et5uql rrkovp55 a8c37x1j keod5gw0 nxhoafnm aigsh9s9 d3f4x2em fe6kdd0r mau55g9w c8b282yb iv3no6db jq4qci2q a3bd9o3v lrazzd5p bwm1u5wc']")
+    sign_in_button.click()
+    print("Clicked on the sign in button")
+    time.sleep(2)
