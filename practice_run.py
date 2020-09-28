f648f690fcb05cbf9ce35c84cb124a2fa2a223d3
Justin Ma <justin.ma8419@gmail.com>
2020-09-28 00:38:13 -0400

    [Justin] modified def location_box to find location by zip code. Added the following def: Publish_button, listing_verification_url, listing_verification_title, remove_listing, removal_verification_title, close_browser.

--- practice_run.py
+++ practice_run.py
@@ -3,17 +3,17 @@ from web_functions import *
 url = data['url']
 username = data['username']
 password = data['password']
-photo_file_path = "C:/Dev/facebook_marketplace/data/Mouse.jpg"
+photo_file_name = 'Mouse.jpg'
 title = 'Broken old computer mouse'
 price = '100'
 condition = 'New'
-zipcode = "10032"
+zipcode = "11205"
 
 launching_website(url)
 sign_in(username, password)
 create_new_listing()
 item_for_sale()
-upload_photos(photo_file_path)
+upload_photos(photo_file_name)
 title_box(title)
 price_box(price)
 category_list()
@@ -24,3 +24,10 @@ subcat3_mice()
 condition_list(condition)
 next_button()
 location_box(zipcode)
+publish_button()
+listing_verification_url()
+listing_verification_title(title)
+remove_listing()
+removal_verification_title(title)
+close_browser()
+
--- web_functions.py
+++ web_functions.py
@@ -12,7 +12,8 @@ chrome_options = webdriver.ChromeOptions()
 prefs = {"profile.default_content_setting_values.notifications": 2}
 chrome_options.add_experimental_option("prefs", prefs)
 
-driver = webdriver.Chrome(chrome_options=chrome_options)
+driver = webdriver.Chrome(options=chrome_options)
+
 driver.implicitly_wait(20)
 driver.maximize_window()
 
@@ -49,17 +50,18 @@ def sign_in(username, password):
     pw.send_keys(Keys.RETURN)
     print("Password is entered")
 
-    """Clicking on the sign in button"""
-    sign_in_button = driver.find_element_by_xpath(
-        "//*[@id='login_form']//span[@class='a8c37x1j ni8dbmo4 stjgntxs l9j0dhe7 ltmttdrg g0qnabr5']")
-    sign_in_button.click()
-    print("Clicked on the sign in button")
-    time.sleep(2)
+    # """Clicking on the sign in button"""
+    # sign_in_button = driver.find_element_by_xpath(
+    #     "//*[@id='login_form']//span[@class='a8c37x1j ni8dbmo4 stjgntxs l9j0dhe7 ltmttdrg g0qnabr5']")
+    # sign_in_button.click()
+    # print("Clicked on the sign in button")
+    # time.sleep(2)
 
 
 def create_new_listing():
     """Select the 'Create New Listing' button"""
-    new_listing = driver.find_element_by_xpath("//a[@aria-label='Create New Listing']//span[contains(text(),'Create New Listing')]")
+    new_listing = driver.find_element_by_xpath(
+        "//*[@id='mount_0_0']/div/div[1]/div[1]/div[3]/div/div/div[1]/div[1]/div[1]/div/div[3]/div[1]/div[2]/div/div/div[6]/div/span/a/div[1]/div[2]/span/span")
     new_listing.click()
     time.sleep(2)
     print("Click on the 'Create New Listing' button.")
@@ -73,10 +75,11 @@ def item_for_sale():
     print("Choosing the 'Item for Sale' option.")
 
 
-def upload_photos(photo_file_path):
+def upload_photos(photo_file_name):
     """Upload photo file"""
-    image_update = driver.find_element_by_xpath("//input[@class='mkhogb32' and contains(@accept, 'image')]")
-    image_update.send_keys(photo_file_path)
+    upload_xpath = driver.find_element_by_xpath("//input[@class='mkhogb32' and contains(@accept, 'image')]")
+    full_path = f"C:/Dev/facebook_marketplace/data/{photo_file_name}"
+    upload_xpath.send_keys(full_path)
     time.sleep(3)
     print("Photo file is uploaded.")
 
@@ -161,23 +164,73 @@ def condition_list(condition):
 
 
 def next_button():
-    driver.find_element_by_link_text(
-        "//span[@class='a8c37x1j ni8dbmo4 stjgntxs l9j0dhe7 ltmttdrg g0qnabr5 ojkyduve']").click()
+    driver.find_element_by_xpath("//div[@aria-label='Next']//span[contains(text(), 'Next')]").click()
     print("Click on the next button")
 
 
 def location_box(zipcode):
     """Finding location by zipcode"""
     postal_code = driver.find_element_by_xpath("//input[@aria-label='Enter a city']")
+    postal_code.send_keys(Keys.CONTROL, "a")
     postal_code.send_keys(zipcode)
+    time.sleep(5)
 
-    """Choose the first result by zipcode"""
-    action = ActionChains(driver)
-    action.send_keys(Keys.ARROW_DOWN).send_keys(Keys.RETURN).perform()
+    """Select the first result by default"""
+    first_option_xpath = f"//ul[@class='buofh1pr cbu4d94t j83agx80']//span[contains(text(),'{zipcode}')]"
+    first_option_element = driver.find_element_by_xpath(first_option_xpath)
+    first_option_element.click()
+    print(f"Selected location by ZipCode: {zipcode}")
 
 
 def publish_button():
     """Click on the publish button to submit the listing"""
-    publish = driver.find_element_by_xpath(
-        "//span[@class='a8c37x1j ni8dbmo4 stjgntxs l9j0dhe7 ltmttdrg g0qnabr5 ojkyduve']")
+    publish = driver.find_element_by_xpath("//div[@aria-label='Publish']//span[contains(text(),'Publish')]")
     publish.click()
+    print("Listing has been published")
+    time.sleep(20)
+
+
+def listing_verification_url():
+    """Verifying if the listing is created successfully by checking the URL"""
+    listing_url = driver.current_url
+    assert listing_url == 'https://www.facebook.com/marketplace/you/selling', 'There is a mismatch in the url, please double check the listing.'
+    print("The URL matches, listing is successfully executed!!")
+    time.sleep(5)
+
+
+def listing_verification_title(title):
+    """Verifying if the listing is created successfully by comparing the title(s) of the active item(s)"""
+    active_item1_xpath = f"//div[@class='tvmbv18p']//span[contains(text(),'{title}')]"
+    active_item1 = driver.find_element_by_xpath(active_item1_xpath)
+    time.sleep(5)
+    assert active_item1.is_displayed()
+    print("Active listing item title is verified")
+
+
+def remove_listing():
+    """Remove the listing and confirm if the removal is being executed"""
+    driver.find_element_by_xpath(
+        "//div[@aria-label='More']//i[@class='hu5pjgll lzf7d6o1 sp_SwTRn0N9BuB sx_3f967d']").click()
+    time.sleep(2)
+    delete_listing_button = driver.find_element_by_xpath("//div[@role='menu']//span[contains(text(),'Delete Listing')]")
+    delete_listing_button.click()
+    time.sleep(1)
+    delete_confirm_button = driver.find_element_by_xpath(
+        "//*[@id='mount_0_0']/div/div[1]/div[1]/div[4]/div/div/div[1]/div/div[2]/div/div/div/div[3]/div[2]/div/div[1]/div[1]/div[1]/div/span/span")
+    delete_confirm_button.click()
+    ignore_survey = driver.find_element_by_xpath(
+        "//div[@aria-label='Close']//div[@class='s45kfl79 emlxlaya bkmhp75w spb7xbtv i09qtzwb n7fi1qx3 b5wmifdl hzruof5a pmk7jnqg j9ispegn kr520xx4 c5ndavph art1omkt ot9fgl3s rnr61an3']")
+    ignore_survey.click()
+    print("Listing has been removed!")
+
+
+def removal_verification_title(title):
+    active_item1_xpath = f"//div[@class='tvmbv18p']//span[contains(text(),'{title}')]"
+    active_item1 = driver.find_element_by_xpath(active_item1_xpath)
+    time.sleep(5)
+    assert not active_item1.is_displayed()
+    print("Listing removal is verified!!")
+
+
+def close_browser():
+    driver.close()
